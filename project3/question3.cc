e0a798fb8c07faab3f702c6ba853b4cc57539b68
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-02-14 11:23:02 -0800

    Finish Question 3

--- project3/question3.cc
+++ project3/question3.cc
@@ -1,7 +1,4 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-// question4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// question3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -26,37 +23,37 @@ void display() {
 }
 
 int main() {
-   _asm {
+    __asm {
         call askData;       // ask user to enter the sentence
-    forLoop:
-        call readChar;
-        cmp letter, '\n';   // if we reach a new line, it means we went through all the letters
+    forLoop:                // forLoop label
+        call readChar;      // Reading character by character from letter
+        cmp letter, ' ';    // compare letter to ' '
+        je forLoop;         // Jump back to for loop label so we can ignore the spaces
+        cmp letter, '.';    // if we reach a period, then we reached the end of the sentence so exit loop
         je exitLoop;
-        cmp letter, 'Z';
-        jle upperCase;
-        cmp letter, 'a';
-        jge lowerCase;
+        cmp letter, 'Z';    // compare the current letter with 'Z'
+        jle upperCase;      // if letter <= 'Z' then jump to upperCase
+        cmp letter, 'a';    // compare the current letter with 'a'
+        jge lowerCase;      // if letter >= 'a' then jump to lowerCase
 
     lowerCase:
-        cmp letter, 'z';
-        jle Increment_L;
+        cmp letter, 'z';    // compare the current letter with 'z'
+        jle Increment_L;    // if letter <= 'z' then this means its a lower case letter so we jump to Increment_L
     upperCase:
-        cmp letter, 'A';
-        jge Increment_U;
+        cmp letter, 'A';    // compare the current letter with 'A'
+        jge Increment_U;    // if letter >= 'A' then this means its a upper case letter so we jump to Increment_U
 
     Increment_U:
-        inc upperCaseCount;
-        jmp forLoop;
+        inc upperCaseCount; // increase the count for upper case letters
+        jmp forLoop;        // jump to for loop
 
     Increment_L:
-        inc lowerCaseCount;
-        jmp forLoop;
+        inc lowerCaseCount; // increase the count for lower case letters
+        jmp forLoop;        // jump to for loop
 
     exitLoop:
-        call display;
+        call display;       // call the display function to print results
 
     }
     return 0;
-} 
-
-
+}
