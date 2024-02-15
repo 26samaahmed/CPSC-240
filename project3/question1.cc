3eecdeaa133e12df917a8e30ee5f35656fb4a9db
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-02-15 08:59:21 -0800

    Finish question1.cc

--- project3/question1.cc
+++ project3/question1.cc
@@ -1,4 +1,4 @@
-/ question1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// question1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
