1f0ba9e1823d42d97232dc587b1200a7b1e3a4d4
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-03-01 20:16:14 -0800

    Finished Question 1

--- project5/question1.cc
+++ project5/question1.cc
@@ -6,11 +6,12 @@ using namespace std;
 
 int total, medium_total, blue_total, i;
 int all_shirts[4][4] = { 10, 20, 30, 40, 
-                  20, 10, 40, 30, 
-                  5, 15, 20, 25, 
-                  30, 25, 20, 15 };
+                         20, 10, 40, 30, 
+                         5, 15, 20, 25, 
+                         30, 25, 20, 15 };
 
 int medium_shirts[4] = { 20, 10, 15, 25 };
+int blue_shirts[4] = { 5, 15, 20, 25 };
 
 void displayTshirtsTotal() {
     cout << "Total Number of all shirts: " << total << endl;
@@ -34,7 +35,7 @@ int main()
         lea esi, [all_shirts];      // esi = [all_shirts]
     forLoop:
         cmp i, 16;                  // compare i and 16
-        Je done;                    // if i == 9, jumpt to done2
+        Je done;                    // if i == 16, jumpt to done
         add eax, [esi];             // eax += all_shirts[][]
         inc i;                      // ++i
         add esi, 4;                 // to go to the next index, add 4 so easi += 4
@@ -43,15 +44,36 @@ int main()
         mov total, eax;             // total = eax
         call displayTshirtsTotal;   
 
-
         // Calculate total of Medium tshirts
         mov eax, 0;                 // eax = 0
         mov i, 0;                   // i = 0
         lea esi, [medium_shirts];   // esi = [medium_shirts]
-
-
+    forLoop2:
+        cmp i, 4;                   // compare i and 4
+        Je done2;                   // if i == 4, jumpt to done2
+        add eax, [esi];             // eax += medium_shirts[]
+        inc i;                      // ++i
+        add esi, 4;                 // go to the next index so esi += 4
+        Jmp forLoop2;
+    done2:
+        mov medium_total, eax;      // medium_total = eax
+        call displayMediumShirts;
 
         // Calculate total of blue tshirts
+        mov eax, 0;                 // eax = 0
+        mov i, 0;                   // i = 0
+        lea esi, [blue_shirts];   // esi = [blue_shirts]
+    forLoop3:
+        cmp i, 4;                   // compare i and 4
+        Je done3;                   // if i == 4, jumpt to done2
+        add eax, [esi];             // eax += medium_shirts[]
+        inc i;                      // ++i
+        add esi, 4;                 // go to the next index so esi += 4
+        Jmp forLoop3;
+    done3:
+        mov blue_total, eax;      // medium_total = eax
+        call displayBlueShirts;
     }
+
     return 0;
 }
