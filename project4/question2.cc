90036129e2e34287728507d6658677802e3db093
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-03-07 11:47:06 -0800

    Fixed Question 2

--- project4/question2.cc
+++ project4/question2.cc
@@ -1,7 +1,5 @@
 // project 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-
 #include <iostream>
 using namespace std;
 
@@ -46,22 +44,19 @@ int main()
         and ax, 0000000000001111b;  // Binary Number for F which is 15
         mov firstChar, ax;          // firstChar = ax which is F
 
-
         mov ax, 0xBEEF;             // ax = 1011111011101111
         and ax, 0000000011110000b;  // Binary Number for E
         shr ax, 4;                  // ax = 0000000000001110
         mov secondChar, ax;         // secondChar = 0000000000001110
-        
 
         mov ax, 0xBEEF;             // ax = 1011111011101111
-        and ax, 0000000011110000b;  // Binary Number for E which is 14
-        shr ax, 4;                  // ax = 0000000000001111
+        and ax, 0000111100000000b;  // Binary Number for E which is 14
+        shr ax, 8;                  // ax = 0000000000001111
         mov thirdChar, ax;          // thirdChar = 0000000000001111
 
-
         mov ax, 0xBEEF;             // ax = 1011111011101111
-        and ax, 0000000000101100b;  // Binary Number for B which is 11
-        shr ax, 2;                  // ax = 0000000000001011
+        and ax, 1111000000000000b;  // Binary Number for B which is 11
+        shr ax, 12;                 // ax = 0000000000001011
         mov fourthChar, ax;
 
         mov ax, firstChar;          // ax = firstChar
@@ -76,34 +71,30 @@ int main()
         cmp sum, 0;                 
         Jne Invalid;                // If they are not equal, jump to Invalid
 
-
     Invalid:
         call displayBeefInvalid;
-
     Valid:
         call displayBeefValid;
-
 
         // FADE
         mov sum, 0;                 // sum = 0;
         mov ax, 0xFADE;             // ax = 1111101011011110
-        and ax, 0000000000001110b;  // Binary Number for E which is 14
+        and ax, 0000000000001111b;  // Binary Number for E which is 14
         mov firstChar, ax;          // firstChar = ax which is E
 
         mov ax, 0xFADE;             // ax = 1111101011011110
-        and ax, 0000000011010000b;  // Binary Number for D which is 13
+        and ax, 0000000011110000b;  // Binary Number for D which is 13
         shr ax, 4;                  // ax = 0000000000001101
         mov secondChar, ax;         // secondChar = ax which is D
 
         mov ax, 0xFADE;             // ax = 1111101011011110
-        and ax, 0000101000000000b;  // Binary Number for A which is 10
+        and ax, 0000111100000000b;  // Binary Number for A which is 10
         shr ax, 8;                  // ax = 0000000000001010
         mov thirdChar, ax;          // thirdtChar = ax which is A
         
-
         mov ax, 0xFADE;             // ax = 1111101011011110
-        and ax, 0000000000011110b;  // Binary Number for F which is 15
-        shr ax, 1;                  // ax = 0000000000001111
+        and ax, 1111000000000000b;  // Binary Number for F which is 15
+        shr ax, 12;                 // ax = 0000000000001111
         mov fourthChar, ax;         // fourthChar = ax which is F
 
         mov ax, firstChar;          // ax = firstChar
@@ -118,35 +109,31 @@ int main()
         cmp sum, 0;
         Jne Invalid2;               // If they are not equal, jump to Invalid
 
-
     Invalid2:
         call displayFadeInvalid;
-
     Valid2:
         call displayFadeValid;
 
         // CABE
         mov sum, 0;                 // sum = 0;
         mov ax, 0xCABE;             // ax = 1100101010111110
-        and ax, 0000000000001110b;  // Binary Number for E which is 14
+        and ax, 0000000000001111b;  // Binary Number for E which is 14
         mov firstChar, ax;          // firsTChar = ax which is E
 
-
         mov ax, 0xCABE;             // ax = 1100101010111110
-        and ax, 0000000010110000b;  // Binary Number for B which is 11
+        and ax, 0000000011110000b;  // Binary Number for B which is 11
         shr ax, 4;                  // ax = 0000000000001011
         mov secondChar, ax;         // secondChar = ax which is B
 
         mov ax, 0xCABE;             // ax = 1100101010111110
-        and ax, 0000001010000000b;  // Binary Number for A which is 10
-        shr ax, 6;                  // ax = 0000000000001010
+        and ax, 0000111100000000b;  // Binary Number for A which is 10
+        shr ax, 8;                  // ax = 0000000000001010
         mov thirdChar, ax;          // thirdChar = ax which is A
 
         mov ax, 0xCABE;             // ax = 1100101010111110
-        and ax, 1100000000000000b;  // Binary Number for C which is 12
+        and ax, 1111000000000000b;  // Binary Number for C which is 12
         shr ax, 12;                 // ax = 0000000000001100
         mov fourthChar, ax;         // fourthChar = ax which is C
-
 
         mov ax, firstChar;          // ax = firstChar
         add ax, secondChar;         // ax = firstChar + secondChar
@@ -162,11 +149,8 @@ int main()
 
     Valid3:
         call displayCabeValid;
-
     Invalid3:
         call displayCabeInvalid;
-
     }
-
     return 0;
 }
