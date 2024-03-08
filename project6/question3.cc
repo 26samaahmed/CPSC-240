3e007e3061a35bf7696c9a7596240ec74a0eefaa
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-03-07 23:46:18 -0800

    Finished Question 3

--- project6/question3.cc
+++ project6/question3.cc
@@ -5,12 +5,12 @@
 #include <iomanip>
 using namespace std;
 
-float num_of_drinks, num_of_sandwiches, sandwich_size, total, counter, drink_total, sandwich_total;
+float num_of_drinks, num_of_sandwiches, sandwich_size, total, counter, drink_total, sandwich_total = 0;
 char  drink_type;
-float soda_price = 2;
-float water_price = 1;
-float ten_inches_price = 3;
-float twelve_inch_price = 5;
+float soda_price = 2.25;
+float water_price = 1.75;
+float ten_inches_price = 3.45;
+float twelve_inch_price = 5.25;
 
 void menu() {
     cout << setfill('-') << setw(10) << '-' << "7-11 Convenience Store" << setfill('-') << setw(10) << '-' << endl;
@@ -33,10 +33,10 @@ void readInput() {
     cin >> num_of_sandwiches;
     cout << "       What size of sandwich (10/12 inches)? ";
     cin >> sandwich_size;
-
 }
 
 void display_total() {
+    cout << fixed << showpoint << setprecision(2);
     cout << "           Your total bill = " << total << endl;
 }
 
@@ -46,8 +46,8 @@ int main()
         call menu;
         call readInput;
         cmp drink_type, 'W';            // comparing the drink type to check if it's equal to 'W'
-        Je Water;                       // If drink_type ='w', jump to Water
-        
+        Je Water;                       // If drink_type ='W', jump to Water
+
     TestWater:
         cmp drink_type, 'w';            // comparing the drink type to check if it's equal to 'w'
         Jne Soda;                       // If drink_type !='w', jump to Soda
@@ -55,7 +55,7 @@ int main()
     Water:
         fld num_of_drinks;              // st[0] = num_of_drinks
         fld water_price;                // st[0] = water_price, st[1] = num_of_drinks
-        fmul;                            // st[0] = num_of_drinks * water_price
+        fmul;                           // st[0] = num_of_drinks * water_price
         fstp drink_total;               // drink_total = num_of_drinks * water_price
         Jmp Sandwich;                   // jump to Sandwich
 
@@ -67,21 +67,20 @@ int main()
 
     Sandwich:
         cmp sandwich_size, 10;          // comparing the sandwich size and the number 10
-        Jne Twelve;                     // if sandwich_size != 10, jump to Twelve
-
-    Ten:
-        fld num_of_sandwiches;          // st[0] = num_of_sandwiches
-        fld ten_inches_price;           // st[0] = num_of_sandwiches , st[1] = num_of_sandwiches
-        fmul;                           // st[0] = num_of_sandwiches * num_of_sandwiches
-        fstp sandwich_total;            // sandwich_total = = num_of_sandwiches * num_of_sandwiches
-        Jmp Total;                      // jump to Total
+        Je Ten;                         // if sandwich_size == 10, jump to Twelve
 
     Twelve:
         fld num_of_sandwiches;          // st[0] = num_of_sandwiches
         fld twelve_inch_price;          // st[0] = twelve_inch_price, st[1] = num_of_sandwiches
         fmul;                           // st[0] = twelve_inch_price * num_of_sandwiches
         fstp sandwich_total;            // sandwich_total =  = twelve_inch_price * num_of_sandwiches
-            
+
+    Ten:
+        fld num_of_sandwiches;          // st[0] = num_of_sandwiches
+        fld ten_inches_price;           // st[0] = num_of_sandwiches , st[1] = num_of_sandwiches
+        fmul;                           // st[0] = num_of_sandwiches * num_of_sandwiches
+        fstp sandwich_total;            // sandwich_total = = num_of_sandwiches * num_of_sandwiches
+
     Total:
         fld drink_total;                // st[0] = drink_total
         fld sandwich_total;             // st[0] = sanwich_total, st[1] = drink_total
